c088da514e667bee28308a392cd842180ae633d1
Roman Nurik <roman@nurik.net>
2016-01-20 02:45:57 -0500

    Update 2016-01-20T07:45:56.001Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";!function(){var e=document.querySelector(".email.obfuscated");if(e.dataset){var t=e.dataset.email;e.setAttribute("href","mailto:"+t.replace(/ /,"@").replace(/ /,".")),console.log(t)}}();
+"use strict";!function(){var e=document.querySelector(".email.obfuscated");if(e.dataset){var t=e.dataset.email;e.setAttribute("href","mailto:"+t.replace(/ /,"@").replace(/ /,"."))}}();
